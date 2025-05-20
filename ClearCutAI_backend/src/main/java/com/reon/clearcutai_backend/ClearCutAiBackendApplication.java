f98888c3eab9df0213a094c964d51d6211751289
Reon Fernandes <reondias@gmail.com>
2025-05-20 15:02:06 +0530

    backend

---
+++ ClearCutAI_backend/src/main/java/com/reon/clearcutai_backend/ClearCutAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.reon.clearcutai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearCutAiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClearCutAiBackendApplication.class, args);
+    }
+
+}
---
+++ ClearCutAI_backend/src/test/java/com/reon/clearcutai_backend/ClearCutAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reon.clearcutai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearCutAiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
